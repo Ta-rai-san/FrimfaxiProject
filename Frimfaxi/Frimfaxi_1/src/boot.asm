1a376a3ec3dd6858367506ccd206301bd7d57b87
Taraisan <necotaraimusic@gmail.com>
2020-12-20 17:20:33 +0900

    Update imgs

--- Frimfaxi/Frimfaxi_1/src/boot.asm
+++ Frimfaxi/Frimfaxi_1/src/boot.asm
@@ -6,27 +6,43 @@
 [BITS 16]
 ORG 0x7C00
 
-BOOT:
-ImageName		DB "Bood-bye Small World", 0x00
-MOV	SI, ImageName
-CALL	DisplayMessage	
+%include "Print.inc"
 
-DisplayMessage:
-	PUSH	AX
-	PUSH	BX
-StartDispMsg:
-	LODSB
-	OR	AL, AL
-	JZ	.DONE
-	MOV	AH, 0x0E
-	MOV	BH, 0x00
-	MOV	BL, 0x07
-	INT	0x10
-	JMP	StartDispMsg
-.DONE:
-	POP	BX
-	POP	AX
-	RET
+msgphello		DB 0x0D, 0x0A, "Hello", 0x0D, 0x0A, 0x00
+
+;==================================================================================
+; BIOS parameter blocks(FAT12)
+;==================================================================================
+JMP	BOOT		;BS_jmpBoot
+BS_jmpBoot2	DB	0x90
+BS_OEMName	DB	"Frmfxi  "
+BPB_BytsPerSec	DW	0x0200		;BytesPerSector
+BPB_SecPerClus	DB	0x01		;SectorPerCluster
+BPB_RsvdSecCnt	DW	0x0001		;ReservedSectors
+BPB_NumFATs	DB	0x02		;TotalFATs
+BPB_RootEntCnt	DW	0x00E0		;MaxRootEntries
+BPB_TotSec16	DW	0x0B40		;TotalSectors
+BPB_Media	DB	0xF0		;MediaDescriptor
+BPB_FATSz16	DW	0x0009		;SectorsPerFAT
+BPB_SecPerTrk	DW	0x0012		;SectorsPerTrack
+BPB_NumHeads	DW	0x0002		;NumHeads
+BPB_HiddSec	DD	0x00000000	;HiddenSector
+BPB_TotSec32	DD	0x00000000	;TotalSectors
+
+BS_DrvNum	DB	0x00		;DriveNumber
+BS_Reserved1	DB	0x00		;Reserved
+BS_BootSig	DB	0x29		;BootSignature
+BS_VolID	DD	0x20202020	;VolumeSerialNumber 日付を入れました
+BS_VolLab	DB	"MyOS       "	;VolumeLabel
+BS_FilSysType	DB	"FAT12   "	;FileSystemType
+
+BOOT:
+ImageName		DB "I am Frimfaxi. ver1.0", 0x00
+MOV	SI, ImageName
+CALL	DisplayMessage
+CALL	Adin
+CLI
+HLT
 
 TIMES 510 - ($ - $$) DB 0
 DW 0xAA55
--- Frimfaxi/Frimfaxi_1/src/make.sh
+++ Frimfaxi/Frimfaxi_1/src/make.sh
@@ -1,3 +1,2 @@
-cd "C:/Program Files/NASM/"
-nasm -o C:/Users/Developer/FrimfaxiProject/Frimfaxi/Frimfaxi_1/src/Intermediateboot.img C:/Users/Developer/FrimfaxiProject/boot.asm
-dd if=C:/Users/Developer/FrimfaxiProject/boot.img bs=512 count=1 of=/proc/sys/Device/ImDisk0
+nasm -o boot.img boot.asm
+dd if=boot.img bs=512 count=1 of=/proc/sys/Device/ImDisk0
